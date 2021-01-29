7623f3294181eb13daedbeeef6d97899fd880b4b
Atfce <36339666+Atfce@users.noreply.github.com>
2021-01-30 00:57:00 +0800

    添加登录和注册表单组件

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import LoginForm from './login/LoginForm.js'
+import RegisterForm from './login/RegisterForm.js'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <RegisterForm />,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
